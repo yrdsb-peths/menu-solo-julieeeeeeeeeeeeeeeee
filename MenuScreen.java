2b7c237f15a92c0d39bb5564dd80db54b865961d
julieeeeeeeeeeeeeeeee <341081727@gapps.yrdsb.ca>
2024-10-15 14:58:17 -0400

    add avatar selection

--- MenuScreen.java
+++ MenuScreen.java
@@ -5,10 +5,16 @@ public class MenuScreen extends World {
         super(600, 400, 1);
         
         addObject(new Button(this::goInstructions), 300, 340);
+        addObject(new Button(this::goAvatar), 300, 225); 
     }
     
     public void goInstructions()
     {
         Greenfoot.setWorld(new InstructionScreen());
+    }
+    
+    public void goAvatar()
+    {
+        
     }
 }
